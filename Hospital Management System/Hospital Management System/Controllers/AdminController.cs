744b72653b34b12fcde1ae31e3f0cd1ef2c573e1
Usman Ali <muausman111@gmail.com>
2018-11-14 07:28:34 +0500

    Hospital Management System

---
+++ Hospital Management System/Hospital Management System/Controllers/AdminController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hospital_Management_System.Models;
+
+namespace Hospital_Management_System.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db;
+
+        //Constructor
+        public AdminController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        //Destructor
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var model = db.Department.ToList();
+            return View(model);
+        }
+
+        //Department Section
+
+        //Department List
+        public ActionResult DepartmentList()
+        {
+            var model = db.Department.ToList();
+            return View(model);
+        }
+
+        //Add Department
+        public ActionResult AddDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddDepartment(Department model)
+        {
+            if (db.Department.Any(c => c.Name == model.Name))
+            {
+                ModelState.AddModelError("Name", "Name already present!");
+                return View(model);
+            }
+            db.Department.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("DepartmentList");
+        }
+
+        //Edit Department
+        public ActionResult EditDepartment(int id)
+        {
+            var model = db.Department.SingleOrDefault(c => c.Id == id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditDepartment(int id, Department model)
+        {
+            var department = db.Department.Single(c => c.Id == id);
+            department.Name = model.Name;
+            department.Description = model.Description;
+            department.Status = model.Status;
+            db.SaveChanges();
+            return RedirectToAction("DepartmentList");
+        }
+
+        public ActionResult DeleteDepartment(int? id)
+        {
+            var department = db.Department.Single(c => c.Id == id);
+            return View(department);
+        }
+
+        [HttpPost, ActionName("DeleteDepartment")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteDepartment(int id)
+        {
+            var department = db.Department.SingleOrDefault(c => c.Id == id);
+            db.Department.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("DepartmentList");
+        }
+
+        //End Department Section
+
+        //Start Ambulance Section
+
+
+        
+    }
+}
---
+++ Hospital Management System/Hospital Management System/Models/Ambulance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hospital_Management_System.Models
+{
+    public class Ambulance
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string AmbulanceId { get; set; }
+        [Required]
+        public string AssignDriver { get; set; }
+
+    }
+}
